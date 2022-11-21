186b47de29f2ec78573894fe2092006363c27bb1
lokeswaran.a <lokeswaran.a@thoughtworks.com>
2022-11-21 16:38:47 +0530

    Add logic for splash screen delay and change splash scfeen

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+    useEffect(() => {
+        setInterval(() => {
+            setAppIsLoaded(true);
+        }, 2000);
+    });
+
+    const onLayout = useCallback(async () => {
+        if (appIsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [appIsLoaded]);
+
+    if (!appIsLoaded) {
+        return null;
+    }
+    return (
+        <View style={styles.container} onLayout={onLayout}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
