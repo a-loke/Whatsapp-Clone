25a8bc3f16b5e6dfc40eaaf76751b65511a86b13
lokeswaran.a <lokeswaran.a@thoughtworks.com>
2022-11-22 18:01:41 +0530

    Fix Keyboard avoid view

--- screens/AuthenticationScreen.js
+++ screens/AuthenticationScreen.js
@@ -1,28 +1,53 @@
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import {
+    Image,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PageContainer from "../components/PageContainer";
 import SignUpForm from "../components/SignUpForm";
 import SignInForm from "../components/SignInForm";
 import colors from "../constants/colors";
+import logo from "../assets/images/logo.png";
 
 const AuthenticationScreen = () => {
     const [isSignedUp, setisSignedUp] = useState(false);
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <PageContainer>
-                {isSignedUp ? <SignUpForm /> : <SignInForm />}
-                <TouchableOpacity
-                    style={styles.linkContainer}
-                    onPress={() => {
-                        setisSignedUp((prevState) => !prevState);
-                    }}
+            <ScrollView>
+                <KeyboardAvoidingView
+                    style={{ flex: 1, justifyContent: "center" }}
+                    behavior={Platform.OS === "ios" ? "height" : undefined}
+                    keyboardVerticalOffset={100}
                 >
-                    <Text style={styles.link}>{`Switch to ${
-                        isSignedUp ? "Sign In" : "Sign Up"
-                    }`}</Text>
-                </TouchableOpacity>
-            </PageContainer>
+                    <PageContainer>
+                        <View style={styles.imageContainer}>
+                            <Image
+                                style={{ width: "50%" }}
+                                source={logo}
+                                resizeMode="contain"
+                            />
+                        </View>
+                        {isSignedUp ? <SignUpForm /> : <SignInForm />}
+                        <TouchableOpacity
+                            style={styles.linkContainer}
+                            onPress={() => {
+                                setisSignedUp((prevState) => !prevState);
+                            }}
+                        >
+                            <Text style={styles.link}>{`Switch to ${
+                                isSignedUp ? "Sign In" : "Sign Up"
+                            }`}</Text>
+                        </TouchableOpacity>
+                    </PageContainer>
+                </KeyboardAvoidingView>
+            </ScrollView>
         </SafeAreaView>
     );
 };
@@ -30,6 +55,10 @@ const AuthenticationScreen = () => {
 export default AuthenticationScreen;
 
 const styles = StyleSheet.create({
+    imageContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
     linkContainer: {
         marginVertical: 15,
         justifyContent: "center",
