b96a1e4c0172344bec5d48ec11b720d27a10e6e3
lokeswaran.a <lokeswaran.a@thoughtworks.com>
2022-11-23 16:23:45 +0530

    Update isAuth value to check redux selector

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,9 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import MainNavigator from "./MainNavigator";
 import AuthenticationScreen from "../screens/AuthenticationScreen";
+import { useSelector } from "react-redux";
 
 export default function AppNavigator() {
-    const isAuth = false;
+    const isAuth = useSelector(
+        (state) => state.auth.token !== null && state.auth.token !== ""
+    );
     return (
         <NavigationContainer>
             {isAuth && <MainNavigator />}
