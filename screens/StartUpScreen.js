d73dfa7be6cfe772511feffa94db2506cf6ee795
lokeswaran.a <lokeswaran.a@thoughtworks.com>
2022-11-23 17:00:18 +0530

    Add logic to check if userData exists in local storage

--- screens/StartUpScreen.js
+++ screens/StartUpScreen.js
@@ -1,9 +1,24 @@
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import commonStyles from "../constants/commonStyles";
 import colors from "../constants/colors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useDispatch } from "react-redux";
+import { setDidTryAutoLogin } from "../store/authSlice";
 
 const StartUpScreen = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const storedData = await AsyncStorage.getItem("userData");
+
+            if (!storedData) {
+                dispatch(setDidTryAutoLogin());
+                return;
+            }
+        };
+        fetchUserData();
+    }, []);
     return (
         <View style={commonStyles.center}>
             <ActivityIndicator size={"large"} color={colors.primary} />
