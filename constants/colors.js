7434f0413244b3fa3209045d91025edca7505b93
lokeswaran.a <lokeswaran.a@thoughtworks.com>
2022-11-25 16:50:01 +0530

    Add search box

--- constants/colors.js
+++ constants/colors.js
@@ -1,5 +1,6 @@
 export default {
     blue: "#3498db",
+    extraLightGrey: "#ededed",
     lightGrey: "#bdc3c7",
     nearlyWhite: "#F4F8F7",
     grey: "#7f8c8d",
--- screens/NewChatScreen.js
+++ screens/NewChatScreen.js
@@ -1,11 +1,16 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { FontAwesome } from "@expo/vector-icons";
+
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import PageContainer from "../components/PageContainer";
+import colors from "../constants/colors";
 
 const NewChatScreen = () => {
     const navigation = useNavigation();
+
     useEffect(() => {
         navigation.setOptions({
             headerLeft: () => {
@@ -20,13 +25,33 @@ const NewChatScreen = () => {
             },
         });
     }, [navigation]);
+
     return (
-        <View>
-            <Text>New Chat screen</Text>
-        </View>
+        <PageContainer>
+            <View style={styles.searchContainer}>
+                <FontAwesome name="search" size={15} color={colors.lightGrey} />
+                <TextInput style={styles.searchBox} placeholder="Search" />
+            </View>
+        </PageContainer>
     );
 };
 
 export default NewChatScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    searchContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: colors.extraLightGrey,
+        height: 30,
+        marginVertical: 8,
+        paddingHorizontal: 8,
+        paddingVertical: 5,
+        borderRadius: 5,
+    },
+    searchBox: {
+        marginLeft: 8,
+        fontSize: 15,
+        width: "100%",
+    },
+});
